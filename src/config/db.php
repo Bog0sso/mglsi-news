6ff6a9053ab3a11c68a75cdd3015b8ec946692df
Bog0sso <sambe2000@hotmail.com>
2022-06-07 10:52:15 +0000

    fix | logging corrected

--- src/config/db.php
+++ src/config/db.php
@@ -11,8 +11,8 @@ try {
   $connexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  // print "Connected successfully";
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  print "Connection failed: " . $e->getMessage();
 }
 ?>
